78275a2c9fb9c22aed3f0050517cc566b7d1dca0
Kobir053 <kobir05333@gmail.com>
2024-07-25 17:23:01 +0300

    Add project files.

---
+++ CarsMVC/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+using CarsMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarsMVC.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ILogger<CarController> _logger;
+
+        public CarController(ILogger<CarController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Model(string name, string year, string color)
+        {
+            CarModel model = new CarModel()
+            {
+                CarName = name,
+                Year = year,
+                Color = color
+            };
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CarsMVC/Models/CarModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarsMVC.Models
+{
+    public class CarModel
+    {
+        public string CarName { get; set; }
+        public string Year { get; set; }
+        public string Color { get; set; }
+    }
+}
